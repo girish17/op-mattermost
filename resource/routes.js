546f5683edf6c72f22917c740c08c856ef1f379b
Girish M <girishm@mailfence.com>
2019-12-05 11:56:51 +0530

    Changing to inline select for project

--- resource/routes.js
+++ resource/routes.js
@@ -17,48 +17,34 @@ module.exports = (app, axios) => {
   });
 
   app.post('/', (req, res) => {
-    const { text, channel_id, user_id, command, trigger_id } = req.body;
-    console.log("Request Body to / ", JSON.stringify(req.body, null, 2));
-    if (text != undefined) {
-      hoursLog = parseFloat(text);
+    const { text, channel_id, user_id, command, trigger_id, token} = req.body;
+    if(token === process.env.MATTERMOST_SLASH_TOKEN)
+    {
+      console.log("Request Body to / ", JSON.stringify(req.body, null, 2));
+      if (text != undefined) {
+        hoursLog = parseFloat(text);
+      }
+      if ((isNaN(hoursLog) || hoursLog < 0.0 || hoursLog > 99.9) && command == "/logtime") {
+        res.send("*0.1 hour to 99.9 hours works well here :) Let's try again...* \n `/logtime [hours]`").status(400);
+      }
+      else {
+        uiActions.showSelProject(req, res, axios);
+      }
     }
-    if ((isNaN(hoursLog) || hoursLog < 0.0 || hoursLog > 99.9) && command == "/logtime") {
-      res.send("*0.1 hour to 99.9 hours works well here :) Let's try again...* \n `/logtime [hours]`").status(400);
-    }
-    else {
-      uiActions.showSelProject(req, res, axios);
+    else
+    {
+      res.send("Invalid request").status(400);
     }
   });
 
 
   app.post('/projSel', (req, res) => {
     console.log("Project dialog submit request: ", req);
-    let txtMsg = {
-      "channel_id": req.body.channel_id,
-      "message": "Test message #testing",
-      "props": {
-        "attachments": [
-          {
-            "pretext": "This is the attachment pretext.",
-            "text": "This is the attachment text."
-          }]
-      }
-    };
-    axios({
-      url: 'posts',
-      method: 'post',
-      baseURL: mmURL,
-      headers: {
-        'Authorization': 'Bearer ' + process.env.MATTERMOST_ACCESS_TOKEN
-      },
-      data: txtMsg
-    }).then((result) => {
-      console.log("response from submit: ", result);
-      res.send().status(200);
-    }).catch((err) => {
-      console.log("error during creating interactive message ", err);
-    });
-    res.send().status(201);
-    //uiActions.loadTimeLogDlg(req, res);
+    uiActions.loadTimeLogDlg(req, res, axios);
+  });
+
+  app.post('/logTime', (req, res) => {
+    console.log("Work package submit request: ", req);
+    res.send("To be done...").send(200);
   });
 }
--- resource/uiActions.js
+++ resource/uiActions.js
@@ -29,45 +29,68 @@ class UIactions {
         });
       });
 
-      var optJSON = JSON.stringify({
-        "trigger_id": req.body.trigger_id,
-        "url": this.intURL + 'projSel',
-        "dialog": {
-          "callback_id": "project_selection",
-          "title": "Select project",
-          "elements": [{
-            "display_name": "Project Selector",
-            "name": "options",
-            "type": "select",
-            "options": optArray
-          }],
-          "submit_label": "Confirm project"
-        }
-      });
+        var optJSON = {
+          "response_type": "in_channel",
+          "message": "Select a project",
+          "props": {
+            "attachments": [
+              {
+                "actions": [
+                  {
+                    "name": "Select a project...",
+                    "integration": {
+                      "url": this.intURL + "projSel",
+                      "context": {
+                        "action": "showTimeLogDlg"
+                      }
+                    },
+                    "type": "select",
+                    "options": optArray
+                  }]
+              }
+            ]
+          }
+        };
 
-      console.log("optArray for projects", optJSON);
-      axios.post(this.mmURL + 'actions/dialogs/open', optJSON).then(response => {
-        console.log("Response from projects dialog: ", response);
-        res.send().status(201);
+        console.log("optArray for projects", optJSON);
+        if(req.body.token === process.env.MATTERMOST_SLASH_TOKEN)
+        {
+          res.set('Content-Type','application/json').send(JSON.stringify(optJSON)).status(200);
+        }
+        else
+        {
+          res.send("Invalid request").status(400);
+        }
+        // axios({
+        //   url: '/posts',
+        //   method: 'post',
+        //   baseURL: this.mmURL,
+        //   headers: {
+        //     'Authorization': 'Bearer: '+process.env.MATTERMOST_ACCESS_TOKEN
+        //   },
+        //   data: optJSON
+        // }).then((response) => {
+        //   console.log("Response from projects select: ", response);
+        //   res.send().status(201);
+        // }).catch(error => {
+        //   console.log("Error while creating projects dialog", error);
+        //   res.send("Select message menu creation failed!").status(500);
+        // })
       }).catch(error => {
-        console.log("Error while creating projects dialog", error);
-        res.send("**Dialog creation failed**").status(500);
-      })
-    }).catch(error => {
-      console.log("Error in getting projects from OP", error);
-      res.send("**Open Project server down!");
+        console.log("Error in getting projects from OP", error);
+        res.send("Open Project server down!!");
     });
   }
 
   loadTimeLogDlg(req, res, axios) {
-    if (req.body.callback_id === "project_selection") {
+    if (req.body.context.action === "showTimeLogDlg") {
       let optArray = [];
       axios({
         url: 'work_packages',
         method: 'get',
         baseURL: this.opURL,
         params: {
-          id: req.body.submission.options.slice(-1)
+          id: req.body.context.selected_option.slice(-1)
         },
         auth: {
           username: 'apikey',
@@ -81,62 +104,86 @@ class UIactions {
             "value": "opt" + element.id
           });
         });
-        /* 
-              var dlgJSON = JSON.stringify({
-                "trigger_id": req.body.trigger_id,
-                "url": this.intURL + 'logTime',
-                "dialog": {
-                  "callback_id": "logTimeDlg",
-                  "title": "Log time for work package",
-                  "elements": [{
-                    "display_name": "Work package selector",
-                    "name": "options",
-                    "type": "select",
-                    "options": optArray
-                  }],
-                  "submit_label": "Log Time"
-                }
-              }); */
 
-        var wpMsgMenu = {
-          "attachments": [
+        var logTimeDlgJSON = JSON.stringify({
+          "trigger_id": req.body.trigger_id,
+          "url": this.intURL + 'logTime',
+          "dialog": {
+            "callback_id": "log_time_dlg",
+            "title": "Log time for work package",
+            "elements": [{
+              "display_name": "Work package",
+              "name": "options",
+              "type": "select",
+              "options": optArray
+            },
             {
-              "pretext": "Work Package Selector",
-              "text": "Select a work package",
-              "actions": [
-                {
-                  "name": "Select an option...",
-                  "integration": {
-                    "url": this.intURL + 'logTime',
-                    "context": {
-                      "action": "showTimeLogDlg"
-                    }
+                "display_name": "Date",
+                "name": "log_date",
+                "type": "text",
+                "placeholder": "YYYY-MM-DD"
+            },
+            {
+              "display_name": "Comment",
+              "name": "comments",
+              "type": "textarea",
+              "help_text": "Please mention comments if any",
+              "optional": true
+            },
+            {
+                "display_name": "Select Activity",
+                "name": "options",
+                "type": "select",
+                "options": [
+                  {
+                    "text": "Development",
+                    "value": "opt3"
                   },
-                  "type": "select",
-                  "options": optArray
-                }
-              ]
+                  {
+                    "text": "Management",
+                    "value": "opt1"
+                  },
+                  {
+                    "text": "Specification",
+                    "value": "opt2"
+                  },
+                  {
+                    "text": "Testing",
+                    "value": "opt4"
+                  },
+                  {
+                    "text": "Support",
+                    "value": "opt5"
+                  },
+                  {
+                    "text": "Other",
+                    "value": "opt6"
+                  },
+                ]
+            },
+            {
+              "display_name": "Billable hours",
+              "type": "text",
+              "subtype": "number"
             }
-          ]
-        }
+          ],
+            "submit_label": "Log time",
+            "notify_on_cancel": true
+          }
+        });
 
-        console.log("optArray for wp", wpMsgMenu);
-        axios.post(this.mmURL + 'posts', {
-          "channel_id": req.body.channel_id,
-          "message": "Select a work package"
-        }/* ,
-          "props": wpMsgMenu */
-        ).then(response => {
-          console.log("Response from log time dialog: ", response);
+        console.log("logTimeDlgJSON: "+logTimeDlgJSON);
+
+        axios.post(this.mmURL + 'actions/dialogs/open', logTimeDlgJSON).then(response => {
+          console.log("Response from projects dialog: ", response);
           res.send().status(201);
-          return;
         }).catch(error => {
-          console.log("Error while creating log time dialog", error);
+          console.log("Error while creating projects dialog", error);
           res.send("**Dialog creation failed**").status(500);
         })
       }, (reason) => {
         console.log("Request failed for /work_packages: %o", reason);
-        this.message.showFailMsg(req, res, axios);
+        //this.message.showFailMsg(req, res, axios);
       });
     }
   }
