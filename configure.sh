9d3ab44e876d530caebcf9eac3f9af93778bdcb6
gm <25923137+girish17@users.noreply.github.com>
2020-05-08 10:47:57 +0530

    removed / from url end
    having / to be typed for every URL seems cumbersome, so removed /

--- configure.sh
+++ configure.sh
@@ -20,17 +20,17 @@ OK="n"
 
 until [ $OK = "y" ]
 do
-    echo '\nPlease enter the following environment variables. Note: URLs must end with /'
-    read -p 'OP_URL (http://localhost:8080/api/v3/): ' OP_URL
+    echo '\nPlease enter the following environment variables'
+    read -p 'OP_URL (http://localhost:8080/api/v3): ' OP_URL
     if [ -z "$OP_URL" ]
     then
-        OP_URL="http://localhost:8080/api/v3/"
+        OP_URL="http://localhost:8080/api/v3"
     fi
     read -p 'INT_URL (enter an http url obtained by running ngrok on 3000 port ): ' INT_URL
-    read -p 'MM_URL (http://localhost:8065/api/v4/): ' MM_URL
+    read -p 'MM_URL (http://localhost:8065/api/v4): ' MM_URL
     if [ -z "$MM_URL" ]
     then
-        MM_URL="http://localhost:8065/api/v4/"
+        MM_URL="http://localhost:8065/api/v4"
     fi
 
     echo "\nPlease enter the generated access tokens for OpenProject and Mattermost"
@@ -53,7 +53,7 @@ do
     fi
 done
 echo '\nGenerating .env file...'
-ENV_CONTENTS='OP_URL='$OP_URL'\nINT_URL='$INT_URL'\nMM_URL='$MM_URL'\nMATTERMOST_ACCESS_TOKEN='$MATTERMOST_ACCESS_TOKEN'\nMATTERMOST_SLASH_TOKEN='$MATTERMOST_SLASH_TOKEN'\nOP_ACCESS_TOKEN='$OP_ACCESS_TOKEN
+ENV_CONTENTS='OP_URL='$OP_URL'/\nINT_URL='$INT_URL'/\nMM_URL='$MM_URL'/\nMATTERMOST_ACCESS_TOKEN='$MATTERMOST_ACCESS_TOKEN'\nMATTERMOST_SLASH_TOKEN='$MATTERMOST_SLASH_TOKEN'\nOP_ACCESS_TOKEN='$OP_ACCESS_TOKEN
 
 touch .env
 echo $ENV_CONTENTS > .env
